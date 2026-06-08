a8aa3f4d091ca2325925d2ee84f0b50f60f760e0
Russell Song Jie Foo <rfoo@c2r7s4.42singapore.sg>
2026-06-08 18:28:57 +0800

    feat: create window for fractol using minilibx

--- include/fractol.h
+++ include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: rfoo <rfoo@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 23:53:25 by rfoo              #+#    #+#             */
-/*   Updated: 2026/06/05 00:00:20 by rfoo             ###   ########.fr       */
+/*   Updated: 2026/06/08 18:28:12 by rfoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@
 # include "mlx.h"
 
 typedef struct s_data
-{
-
+{	
+	int	size_x;
+	int	size_y;
 }	t_data;
+
 
 #endif
--- src/fractol.c
+++ src/fractol.c
@@ -6,7 +6,29 @@
 /*   By: rfoo <rfoo@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/07 19:14:51 by rfoo              #+#    #+#             */
-/*   Updated: 2026/06/08 15:08:52 by rfoo             ###   ########.fr       */
+/*   Updated: 2026/06/08 17:55:46 by rfoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "fractol.h"
+
+void	fractol(int size_x, int size_y, char *title)
+{
+	void	*mlx;
+	void	*window;
+
+	mlx = mlx_init();
+	if (!mlx)
+	{
+		ft_printf("Failed to connect to graphical system.");
+		return ;
+	}
+	window = mlx_new_window(mlx, size_x, size_y, title);
+	if (!window)
+	{
+		ft_printf("Failed to create mlx window.");
+		return ;
+	}
+	mlx_loop(mlx);
+	return ;
+}
