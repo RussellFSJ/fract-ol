f76e056c81381c402e6b1aebef386499eb2cf041
Russell Song Jie Foo <rfoo@c2r7s4.42singapore.sg>
2026-06-11 23:07:31 +0800

    feat: add pixel put and mandelbrot to draw mandelbrot sets

--- src/utils/pixel_put.c
+++ src/utils/pixel_put.c
@@ -1,18 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   draw_fractal.c                                     :+:      :+:    :+:   */
+/*   pixel_put.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rfoo <rfoo@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/10 13:55:57 by rfoo              #+#    #+#             */
-/*   Updated: 2026/06/10 16:15:47 by rfoo             ###   ########.fr       */
+/*   Created: 2026/06/11 21:13:49 by rfoo              #+#    #+#             */
+/*   Updated: 2026/06/11 21:19:22 by rfoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "fractol.h"
+#include "fractol.h"
 
-// void	draw_fractal(t_fractol f)
-// {
-// 	return;
-// }
+void	put_pixel(t_fractol *f, int x, int y, int colour)
+{
+	char	*dest;
+
+	dest = f->address + (y * f->line_len + x * (f->bpp / 8));
+	*(unsigned int*)dest = colour;
+}
